f98db7f3b4508bca16f842000c3e7a888c10441b
venkatb <venkatb@WSHYD-031.WILCO.COM>
2018-11-29 13:14:06 +0530

    v1

--- sample/src/samplepackage/sampleclass.java
+++ sample/src/samplepackage/sampleclass.java
@@ -5,6 +5,7 @@ public class sampleclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Shenba");
+		System.out.println("hello shenba ");
 	}
 
 }
